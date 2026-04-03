bf1b85ceae9645f60f555f956c934bdf2c77ca69
Hunter <huntergraham220@gmail.com>
2026-04-03 17:47:10 -0500

    Remove debug scripts, fix protocol names, complete WFP ctypes implementation

--- check_keys.py
+++
@@ -1,5 +0,0 @@
-import yaml
-config = yaml.safe_load(open('config/default_config.yaml'))
-keys = config['threat_intel']['api_keys']
-print('abuseipdb:', 'SET' if keys.get('abuseipdb') else 'EMPTY')
-print('otx:', 'SET' if keys.get('otx') else 'EMPTY')
